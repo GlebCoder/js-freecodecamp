99fde733568ff82bf3a5ae87542915d7f991320b
GlebCoder <gberliand@gmail.com>
2024-12-09 16:05:50 +0300

    make an inverted pyramid with .unshift()

--- pyramidInverted.js
+++ pyramidInverted.js
@@ -6,12 +6,15 @@ function padRow(rowNum, rowCount) {
     return " ".repeat(rowCount - rowNum) + character.repeat(2 * rowNum - 1) + " ".repeat(rowCount - rowNum);
 }
 
-for (let i = count; i > 0; i--) {
+/*for (let i = count; i > 0; i--) {
     rows.push(padRow(i, count));
-}
+}*/
 /*while (rows.length < count) {
     rows.push(padRow(rows.length + 1, count));
 }*/
+for (let i = 0; i < count; i++) {
+    rows.unshift(padRow(i + 1, count));
+}
 console.log(rows);
 
 let pyramid = "";
