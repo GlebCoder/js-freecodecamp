756fba8dfef4cbbbdd3604582df661c5669874b0
GlebCoder <gberliand@gmail.com>
2024-12-09 13:46:08 +0300

    make the pyramid inverted

---
+++ pyramidInverted.js
@@ -0,0 +1,21 @@
+const character = "#";
+let rows = [];
+const count = 8;
+
+function padRow(rowNum, rowCount) {
+    return " ".repeat(rowCount - rowNum) + character.repeat(2 * rowNum - 1) + " ".repeat(rowCount - rowNum);
+}
+
+for (let i = count; i > 0; i--) {
+    rows.push(padRow(i, count));
+}
+/*while (rows.length < count) {
+    rows.push(padRow(rows.length + 1, count));
+}*/
+console.log(rows);
+
+let pyramid = "";
+for (const row of rows) {
+    pyramid += row + "\n";
+}
+console.log(pyramid);
